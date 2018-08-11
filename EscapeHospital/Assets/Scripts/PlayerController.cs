5dcf288a7cc996177186d4f8fa2a747cb1d96cb0
bronzehanzo <kcrobert@naver.com>
2018-08-11 15:00:58 +0900

    no message

---
+++ EscapeHospital/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D playerRigidBody;
+    CapsuleCollider2D playerCollider;
+
+    public float maxSpeed = 100f;
+
+    float hSpeed = 0f;
+
+    bool facingRight = true;
+
+    private void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        //playerCollider = GetComponent<CapsuleCollider>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Translate(x, 0, 0);
+    }
+}
