62a0e32995c2ace04ebc843ed653c4fba8b45b1e
PlayerAnonoymous <wndnjs040774@naver.com>
2018-08-25 16:59:23 +0900

    InteractiveStuff(항아리, 쓰레기통, 서랍)앞에서 'space'를 눌렀을때 겉에있는 항아리, 쓰레기통, 서랍이 깨지고 그 속에있는 아이템이 나오도록 함.

---
+++ EscapeHospital/Assets/Scripts/InteractiveStuff.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveStuff : MonoBehaviour
+{
+
+    public GameObject interactiveStuff;
+    public GameObject included;
+
+    public void Start()
+    {
+        included.SetActive(false);
+    }
+ 
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                interactiveStuff.SetActive(false);
+                included.SetActive(true);
+            }
+        }
+    }
+
+}
---
+++ EscapeHospital/Assets/Scripts/Key.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+
+    public GameObject player;
+
+    bool KeyIsActive;
+
+    // Use this for initialization
+    private void Start()
+    {
+        KeyIsActive = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (KeyIsActive)
+        {
+        }
+    }
+}
